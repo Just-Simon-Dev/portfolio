8880cccc67765e89c5daf3b167ce7a78444d131d
Just-Simon-Dev <szymonsoborowski@gmail.com>
2024-08-04 18:14:06 +0200

    Create a responsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainViewComponent} from "./main-view/main-view.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainViewComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
